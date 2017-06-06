e4889da73daea29c3baa896322401bba0bdc15da
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-06-06 17:40:17 +0000

    add list_park method and start method with fake code to test interface

--- lib/national_parks/cli.rb
+++ lib/national_parks/cli.rb
@@ -1,5 +1,25 @@
 class NationalParks::CLI
   def call
-    puts "national parks list"
-  end
-end
+    list_parks
+    start
+  end #call
+
+  def list_parks
+    puts "A list of all the parks sorted by states."
+  end #list_parks
+
+  def start
+    puts "test test"
+    input = nil
+    while input != "exit" 
+      puts "enter the name of the state you would like to visit or enter exit:"
+      input = gets.strip.downcase
+      case input
+      when "alaska"
+        puts "numbered list of the parks in Alaska."
+      when "american samona"
+        puts "numbered list of parks in american samona"
+      end #case input
+    end #while
+  end#start
+end #NationalParks::CLI
